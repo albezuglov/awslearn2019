c087cd781e2852e3ccdf690ce21a49733e9dd3f6
albezuglov <alexworldmail@gmail.com>
2019-05-06 18:55:23 +0300

    Added init script

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt update && apt install vim mc apache2 -y
+echo "Hello world from $(hostname)<br><table><tr><td style='padding-right: 30px;' >Public hostname: </td><td>$(curl http://169.254.169.254/latest/meta-data/public-hostname)</td></tr><tr><td>Public IP: </td><td>$(curl http://169.254.169.254/latest/meta-data/public-ipv4)</td></tr><tr><td>Availability zone: </td><td>$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)</td></tr></table>" > /var/www/html/index.html
